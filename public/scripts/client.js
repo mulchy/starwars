d84a2c9ef4debc740b577f39187c922665acc98e
Ryan Mulcahy <ryan@primeacademy.io>
2016-10-19 15:38:34 -0500

    Add scrolling star wars text.

---
+++ public/scripts/client.js
@@ -0,0 +1,28 @@
+var app = angular.module('starWarsApp', []);
+
+app.controller('MainController', MainController);
+
+var API = 'http://swapi.co/api';
+
+function MainController($http) {
+  var main = this;
+  console.log('MainController loaded');
+
+  main.films = [];
+  main.lines = [];
+  main.getFilmData = function() {
+    main.lines = [];
+    main.class = '';
+    $http.get(main.selected)
+         .then(function(response) {
+           main.lines = response.data.opening_crawl.split('\n');
+           main.class = 'scroll';
+         });
+  };
+
+  $http.get(API + '/films')
+       .then(function(response){
+         console.log('response', response);
+         main.films = response.data.results;
+       });
+}
